5a8a60734ab9c0f54b5574a8e4fc9319df0463d0
treerajeat <ta_acs2@hotmail.com>
2020-03-05 14:46:50 +0700

    test postges4

---
+++ app/project2.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class project extends Model
+{
+    protected $table = 'park2';
+    protected $fillable = ['detect','time'];
+}
