0b29f364db569bb6693d0470f46b01f157398452
treerajeat <ta_acs2@hotmail.com>
2020-03-10 19:27:58 +0700

    testajax1

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -177,7 +177,7 @@
   {{-- <img name="main" id="main" width="700" height="400" src="http://192.168.43.150:58545/videostream.cgi?user=admin&pwd=TApop123"> --}}
     {{-- <h1 id="activity"></h1> --}}
     {{-- <input type='button' value='SHITTY BUTTON' id='but_fetchall' > --}}
-    <table id='userTable'class="container-fluid bg-1 text-center" style="font-size:20%">
+    <table id='userTable'class="container-fluid bg-1 text-center" style="font-size:1000ch">
      <thead>
       <tr>
         <th>จำนวนที่จอดรถทั้งหมด 8</th>
