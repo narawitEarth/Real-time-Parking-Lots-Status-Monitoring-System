d1eda5afe419c2f603b9fc37ffad803bea1c3f3d
treerajeat <ta_acs2@hotmail.com>
2020-03-10 15:03:15 +0700

    testajax1

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -10,6 +10,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   <link href="{{ asset('css/view.css') }}" rel="stylesheet">
+  <script src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js">
 
 
   <style>
