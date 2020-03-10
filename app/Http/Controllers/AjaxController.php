b247b01009321ea65030d9036f8de68ab23f560b
treerajeat <ta_acs2@hotmail.com>
2020-03-10 14:59:48 +0700

    testajax1

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
 
-class AjaxController extends Controller
-{
-    //
+class AjaxController extends Controller {
+   public function index() {
+      $msg = "This is a simple message.";
+      return response()->json(array('msg'=> $msg), 200);
+   }
 }
