47f4668d1d69d4f552a29ea8a16a99060a90a543
treerajeat <ta_acs2@hotmail.com>
2020-03-17 01:50:48 +0700

    testrespon

--- resources/views/view2.blade.php
+++ resources/views/view2.blade.php
@@ -150,6 +150,7 @@
 </head>
 <body>
 <!-- Navbar -->
+<a class="w3-button w3-padding-16 w3-left" onclick="w3_open()">☰</a>
 <nav class="navbar">
   <div class="container" id="navbar">
     <div class="navbar-header">
@@ -174,8 +175,8 @@
 <nav class="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left" style="display:none;z-index:2;width:40%;min-width:300px" id="mySidebar">
     <a href="javascript:void(0)" onclick="w3_close()"
     class="w3-bar-item w3-button">Close Menu</a>
-        <a href="{{route('project.view')}}">ATADA LOT1 ||</a>
-        <a href="{{route('project.view2')}}">ATADA LOT2 ||</a>
+    <a href="{{route('project.view')}}">ATADA LOT1 ||</a>
+    <a href="{{route('project.view2')}}">ATADA LOT2 ||</a>
 
 </nav>
 <!-- First Container -->
