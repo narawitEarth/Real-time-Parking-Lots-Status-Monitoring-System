21ba4971f5a41c0e35a19ce2488c26fa3a860ac2
treerajeat <ta_acs2@hotmail.com>
2020-03-17 00:48:03 +0700

    testrespon

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -24,7 +24,7 @@
   p {font-size: 16px;}
   }
   @media only screen and (min-width: 600px){
-  .margin {margin-bottom: 45px;}
+  .margin {margin-bottom: 40px;}
   }
   @media only screen and (min-width: 600px){
   .bg-1 {
