9c5b6283e49c52a48ea746c101b18ec6fdc7178b
treerajeat <ta_acs2@hotmail.com>
2020-03-17 01:59:20 +0700

    testrespon

--- resources/views/view2.blade.php
+++ resources/views/view2.blade.php
@@ -173,7 +173,7 @@
 
 <nav class="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left" style="display:none;z-index:2;width:40%;min-width:300px" id="mySidebar">
     <a href="javascript:void(0)" onclick="w3_close()"
-    class="w3-bar-item w3-button">Close Menu ||</a>
+    class="w3-bar-item w3-button" style="margin-left:5%">Close Menu ||</a>
     <a href="{{route('project.view')}}">ATADA LOT1 ||</a>
     <a href="{{route('project.view2')}}">ATADA LOT2 ||</a>
 
