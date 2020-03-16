7f7c7924340186a94b97f9dc35153d7bf93bd2c3
treerajeat <ta_acs2@hotmail.com>
2020-03-17 00:45:07 +0700

    testrespon

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -207,7 +207,7 @@
 @foreach ($park as $p)
 <div class="container-fluid bg-1 text-center" id="vd">
     <h1 class="margin" style="font-size:1cm">ลานจอดรถหอพัก ATADA 1</h1>
-    <iframe width="450" height="450" src="https://www.youtube.com/embed/cIzbqRLO74E?channel=UCV1aXH_T6lQ-il9ewg9NMxg&autoplay=1&mute=1&enablejsapi=1" frameborder="0" allowfullscreen></iframe>
+    <iframe width="200" height="300" src="https://www.youtube.com/embed/cIzbqRLO74E?channel=UCV1aXH_T6lQ-il9ewg9NMxg&autoplay=1&mute=1&enablejsapi=1" frameborder="0" allowfullscreen></iframe>
   {{-- <video width="500" height="300" autoplay="autoplay">
         <source src="assets\img\upload\vdo.mp4"  />
   </video> --}}
