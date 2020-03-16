1f522969d870de586b72386104ee8d687da10f0b
treerajeat <ta_acs2@hotmail.com>
2020-03-17 01:44:15 +0700

    testrespon

--- resources/views/view2.blade.php
+++ resources/views/view2.blade.php
@@ -149,12 +149,6 @@
     </script>
 </head>
 <body>
-<nav class="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left" style="display:none;z-index:2;width:40%;min-width:300px" id="mySidebar">
-        <a href="javascript:void(0)" onclick="w3_close()"
-        class="w3-bar-item w3-button">Close Menu</a>
-        <a href="#food" onclick="w3_close()" class="w3-bar-item w3-button">Food</a>
-        <a href="#about" onclick="w3_close()" class="w3-bar-item w3-button">About</a>
-    </nav>
 <!-- Navbar -->
 <nav class="navbar">
   <div class="container" id="navbar">
@@ -177,6 +171,12 @@
   </div>
 </nav>
 
+<nav class="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left" style="display:none;z-index:2;width:40%;min-width:300px" id="mySidebar">
+    <a href="javascript:void(0)" onclick="w3_close()"
+    class="w3-bar-item w3-button">Close Menu</a>
+    <a href="#food" onclick="w3_close()" class="w3-bar-item w3-button">Food</a>
+    <a href="#about" onclick="w3_close()" class="w3-bar-item w3-button">About</a>
+</nav>
 <!-- First Container -->
 <div class="sidenav">
   <a href="{{route('project.view')}}">ATADA LOT1 ||</a>
