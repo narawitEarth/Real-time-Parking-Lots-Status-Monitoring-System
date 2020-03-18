36934986d3dd62b3efc407acb54f9c6674f054b5
treerajeat <ta_acs2@hotmail.com>
2020-03-18 11:32:45 +0700

    testpro

--- resources/views/view2.blade.php
+++ resources/views/view2.blade.php
@@ -158,7 +158,6 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-    <a class="navbar-brand" onclick="w3_open()">☰</a>
     <a class="navbar-brand" href="{{route('project.view')}}">PARKING</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
