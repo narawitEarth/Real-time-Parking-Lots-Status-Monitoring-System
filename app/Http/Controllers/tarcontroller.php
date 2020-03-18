7870c3cd3709ac6815510f76d95854d1b379fced
treerajeat <ta_acs2@hotmail.com>
2020-03-18 16:18:08 +0700

    testrespon

--- app/Http/Controllers/tarcontroller.php
+++ app/Http/Controllers/tarcontroller.php
@@ -38,7 +38,7 @@ class tarcontroller extends Controller
         exit;
     }
     public function logData(){
-        $logA['data'] = project::orderBy('id','desc')->get();
+        $logA['data'] = project::orderBy('id','desc')->limit(10)->get();
         // return view('view',[
         //     'park' => $parking
         // ]);
